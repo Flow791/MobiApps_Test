4e197d41e7c52801218dba961b256950aa7d4c6a
Flow791 <Flow791@outlook.fr>
2017-10-13 23:37:00 +0200

    Modification du type de controller

--- MobiApp_Test/ListePokemonController.swift
+++ MobiApp_Test/ListePokemonController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ListePokemonController: UIViewController {
+class ListePokemonController: UITableViewController {
     
     private let pokemonManager:PokemonManager = PokemonManager()
     private var pokemons:[Pokemon] = [Pokemon]()
@@ -29,15 +29,5 @@ class ListePokemonController: UIViewController {
         }
         
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
